61f96832fc25ebbd105f257eacaa0e3c0bf01890
song <943096163@qq.com>
2021-03-22 14:27:17 +0800

    Redis

--- 3.Redis/redistest/src/main/java/com/test/controller/SentinelTestController.java
+++ 3.Redis/redistest/src/main/java/com/test/controller/SentinelTestController.java
@@ -3,6 +3,7 @@ package com.test.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +13,16 @@ import java.util.Date;
 @Slf4j
 public class SentinelTestController {
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate stringRedisTemplate;
 
     @RequestMapping("/testSet")
     public Object test() {
-        redisTemplate.opsForValue().set("time", new Date().getTime()+"", 100000);
+        stringRedisTemplate.opsForValue().set("time", new Date().getTime()+"", 100000);
         return "succsee";
     }
     @RequestMapping("/testGet")
     public Object testGet() {
-        return redisTemplate.opsForValue().get("time");
+        return stringRedisTemplate.opsForValue().get("time");
     }
+
 }
